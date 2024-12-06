4a131474f12ddfe03d44a9eaa59cd72b74336470
martijndoes <martijn@vandoesselaar.nl>
2024-12-06 18:53:48 +0100

    Added commentary and made code better to read

--- MMM-Pollen.js
+++ MMM-Pollen.js
@@ -4,11 +4,11 @@ Module.register("MMM-Pollen", {
 		  mode: "table",						//table will display simple table. Map will display google maps with overlay
           googleApiKey: "[YOURAPIKEY]",			//your google api key
 		  zoomLevel: 9,							//will control the zoomlevel if mode = map
-          longitude: 52.3108558,
-          latitude: 4.950632,
-          language: "nl",
-          days: 5,                              //number of forecast days you want to display in table view
-          updateInterval: 60,					//in minutes. How often will it retrieve new info from the API.
+          longitude: 52.3108558,				//longitude of your location
+          latitude: 4.950632,					//latitude of your location
+          language: "en",						//language in which labels are displayed
+          days: 3,                              //number of forecast days you want to display in table view
+          updateInterval: 60,					//in minutes. How often will it retrieve new info from the API. Don't set it too low, it will drain your monthly API Calls
 		  rotateInterval: 10					//in seconds. How many seconds between pollentype switches in the map
     },
 	
@@ -34,6 +34,7 @@ Module.register("MMM-Pollen", {
 		]
 	},	
 
+	//start the dom update interval
     start: function()
     {
         setInterval(() => {
@@ -45,6 +46,7 @@ Module.register("MMM-Pollen", {
     getDom: async function ()
     {
 		var self=this;
+		//if mode is set to mep retrieve all the information needed for map display
 		if(this.config.mode === "map")
 		{
 			var wrapper = document.createElement("div");
@@ -52,13 +54,14 @@ Module.register("MMM-Pollen", {
 			wrapper.appendChild(script);
 			var key = this.config.googleApiKey
 			//thanks to https://forum.magicmirror.builders/topic/716/can-t-load-script-correctly/2 @ianperrin for this solution!
+			//getting the script. The callback initmap function is called after the map is loaded
 			this.getScript('https://www.google.com/jsapi', function() { 
 				google.load('maps', '3', { other_params: 'key=' + key, callback: function() {
 					self.initMap();
 				}});
 			});
 			
-			
+			//create three divs. One for each type of map
 			var map = document.createElement("div");
 			var id = document.createAttribute("id");
 			id.value = "mapDiv";
@@ -77,8 +80,10 @@ Module.register("MMM-Pollen", {
 			map3.setAttributeNode(id3);
 			wrapper.appendChild(map3);
 		}
+		//what happens is the display mode is set to table
 		else if (this.config.mode === "table")
 		{
+			//first construct thr url and then retrieve the table with all the data
 			var api_url = this.constructUrl();
 			var dataTable = await this.getData(api_url);
 			var wrapper = document.createElement("div");
@@ -104,7 +109,7 @@ Module.register("MMM-Pollen", {
   
             const json = await response.json();
 			
-			//header
+			//header of the table. Fill it with the types of pollen
 			var label1 = json.dailyInfo[0].pollenTypeInfo[0].displayName;
 			var label2 = json.dailyInfo[0].pollenTypeInfo[1].displayName;
 			var label3 = json.dailyInfo[0].pollenTypeInfo[2].displayName;
@@ -127,20 +132,18 @@ Module.register("MMM-Pollen", {
 			
 			var dayEl = json.dailyInfo;
             var dayLen = dayEl.length;
-			
+			//loop through the days
 			for(var d = 0; d < dayLen; d++)
 			{
 				var el = json.dailyInfo[d].pollenTypeInfo;
 				var len = el.length;
-				
-				
-				
+				//create the row, fill the first cell with the name of the day
 				var row = document.createElement("tr");
 				var dayCell = document.createElement("td");
 				dayCell.innerHTML = this.getDayName(d, this.config.language);
 				row.appendChild(dayCell);
 				
-				
+				//loop through the pollentypes. Fill the cell with the value for that day
 				for (var i = 0; i < len; i++)
 				{
 					var entry = json.dailyInfo[d].pollenTypeInfo[i];
@@ -190,7 +193,7 @@ Module.register("MMM-Pollen", {
         return url;
     },
 	
-	
+	//gets the name of the day in a specific language, either the current or "addDays" days in future.
 	getDayName: function(addDays, locale)
 	{
 		var date = new Date();
@@ -202,8 +205,10 @@ Module.register("MMM-Pollen", {
 	//init the map view
 	initMap: function() 
 	{
+		//set the location from the config
 		const myLatLng = { lat: this.config.latitude, lng: this.config.longitude };
 
+		//initialize the maps. Some configs are in here like the zoomlevel
 		const map = new google.maps.Map(document.getElementById("mapDiv"), {
 		  mapId: "ffcdd6091fa9fb03",
 		  zoom: this.config.zoomLevel,
@@ -246,22 +251,24 @@ Module.register("MMM-Pollen", {
 		  disableDefaultUI: true,
 		});
 
+		//initialize the overlays and insert them in the specific maps
 		const pollenMapTypeGrass = new PollenMapType(new google.maps.Size(256, 256),"Grass_UPI", this.config.googleApiKey);
 		const pollenMapTypeTree = new PollenMapType(new google.maps.Size(256, 256),"Tree_UPI", this.config.googleApiKey);
 		const pollenMapTypeWeed = new PollenMapType(new google.maps.Size(256, 256),"Weed_UPI", this.config.googleApiKey);
-		
 		map.overlayMapTypes.insertAt(0, pollenMapTypeGrass);
 		map2.overlayMapTypes.insertAt(0, pollenMapTypeTree);
 		map3.overlayMapTypes.insertAt(0, pollenMapTypeWeed);
 		
+		//gets the divs and hides two of them
 		var divMap = document.getElementById('mapDiv');
 		var divMap2 = document.getElementById('mapDiv2');
 		var divMap3 = document.getElementById('mapDiv3');
-		
 		divMap.style.display = "none";
 		divMap2.style.display = "none";
 		divMap3.style.display = "block";
 		
+
+		//rotate between the three maps every "rotateInterval" seconds. Update the header to show which maps is being shown.
 		var i = 1;
 		var el = document.querySelector(".MMM-Pollen .module-header");
 		el.innerHTML = this.getHeader() + " - " + this.translate("Grass");
@@ -298,7 +305,7 @@ Module.register("MMM-Pollen", {
         }, this.config.rotateInterval * 1000);
 	},
 	
-	
+	//gets a JS file and loads it
 	getScript: function(source, callback) {
       var script = document.createElement('script');
       var prior = document.getElementsByTagName('script')[0];
